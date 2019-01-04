4eb42baa0890b03597c01340fb25d12455184c98
Robin Wieruch <wrobin@gmx.net>
2019-01-04 22:19:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Simple Node with Express + PostgreSQL Server
+
+[![Build Status](https://travis-ci.org/rwieruch/node-express-postgresql-server.svg?branch=master)](https://travis-ci.org/rwieruch/node-express-postgresql-server) [![Slack](https://slack-the-road-to-learn-react.wieruch.com/badge.svg)](https://slack-the-road-to-learn-react.wieruch.com/) [![Greenkeeper badge](https://badges.greenkeeper.io/rwieruch/node-express-postgresql-server.svg)](https://greenkeeper.io/)
+
+An easy way to get started with a Express server with PostgreSQL with Node.js.
+
+## Features
+
+* Babel 7
+* Environment Variables
+* Express
+* PostgreSQL
+
+## Requirements
+
+* [node & npm](https://nodejs.org/en/)
+* [git](https://www.robinwieruch.de/git-essential-commands/)
+
+## Installation
+
+* `git clone git@github.com:rwieruch/node-express-postgresql-server.git`
+* `cd node-express-postgresql-server`
+* `npm install`
+* `npm start`
+
+### GET Routes
+
+* visit http://localhost:3000
+  * /messages
+  * /messages/1
+  * /users
+  * /users/1
+
+### Beyond GET Routes
+
+#### CURL
+
+* Create a message with:
+  * `curl -i -X POST -H "Content-Type:application/json" http://localhost:3000/messages -d '{"text":"Hi again, World"}'`
+* Delete a message with:
+  * `curl -i -X DELETE -H "Content-Type:application/json" http://localhost:3000/messages/1`
+
+#### Postman
+
+* Install [Postman](https://www.getpostman.com/apps) to interact with REST API
+* Create a message with:
+  * URL: http://localhost:3000/messages
+  * Method: POST
+  * Body: raw + JSON (application/json)
+  * Body Content: `{ "text": "Hi again, World" }`
+* Delete a message with:
+  * URL: http://localhost:3000/messages/1
+  * Method: DELETE
---
+++ src/index.js
@@ -0,0 +1,105 @@
+import 'dotenv/config';
+import cors from 'cors';
+import uuidv4 from 'uuid/v4';
+import bodyParser from 'body-parser';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+
+// Postman: x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Postman: raw + JSON (application/json)
+app.use(bodyParser.json());
+
+// Sample Data
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+    messageIds: [1],
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+    messageIds: [2],
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+// Application-Level Middleware
+
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
+
+// Routes
+
+app.get('/me', ({ me }, res) => {
+  return res.send(users[me.userId]);
+});
+
+app.get('/users', (req, res) => {
+  return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+app.post('/messages', ({ body: { text }, me }, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: text,
+    userId: me.id,
+  };
+
+  messages[id] = message;
+  users[me.id].messageIds.push(id);
+
+  return res.send(message);
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = messages;
+
+  if (!message) {
+    return res.send(false);
+  }
+
+  messages = otherMessages;
+
+  return res.send(true);
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
